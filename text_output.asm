65f462a5826ff9ebc4c96483b99191169ed97a3d
57696E746572 <57696E746572@gmail.com>
2025-11-01 23:47:05 +0000

    text_output.asm

--- text_output.asm
+++ text_output.asm
@@ -6,14 +6,14 @@ section .text
     global _start
 
 _start:
-    ; write(1, msg, len)
+    
     mov rax, 1
     mov rdi, 1
     mov rsi, msg
     mov rdx, len
     syscall
 
-    ; exit(0)
+    
     mov rax, 60
     xor rdi, rdi
     syscall
